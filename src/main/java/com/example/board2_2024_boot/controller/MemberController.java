85d65078edfea91872d2ae020652d70ea7025bba
SHIN SUJUNG <shinssjj79@gmail.com>
2024-08-02 21:29:22 +0900

    Refactored MemberController to use MemberService for saving member data
    - Introduced dependency injection via constructor for MemberService.
    - Added MemberService as a final field in MemberController.
    - Modified the save method to delegate saving functionality to MemberService.

--- src/main/java/com/example/board2_2024_boot/controller/MemberController.java
+++ src/main/java/com/example/board2_2024_boot/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.example.board2_2024_boot.controller;
 
 import com.example.board2_2024_boot.dto.MemberDTO;
+import com.example.board2_2024_boot.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,18 +10,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MemberController {
+@RequiredArgsConstructor
+public class MemberController{
+    //생성자 주입
+    private final MemberService memberService;
+
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
-    public String saveForm() {
-        return "save";
-    }
+    public String saveForm() {return "save";}
 
     @PostMapping("/member/save")
     public String save(@ModelAttribute MemberDTO memberDTO){
-        System.out.println("MemberController.save");
-        System.out.println("memberDTO = " + memberDTO);
-        return "index";
-    }
-
+            System.out.println("MemberController.save");
+            System.out.println("memberDTO = " + memberDTO);
+            memberService.save(memberDTO);
+            return "index";
+        }
 }
