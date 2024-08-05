93218cf1ddde9dcc06bb67ba085e928e6515f29d
SHIN SUJUNG <shinssjj79@gmail.com>
2024-08-05 20:05:27 +0900

    feat: Add MemberDTO and MemberRepository to MemberService
    - Added import for MemberDTO and MemberRepository.
    - Annotated MemberService with @RequiredArgsConstructor for dependency injection.
    - Introduced memberRepository as a final member.
    - Added save method to handle MemberDTO.

--- src/main/java/com/example/board2_2024_boot/service/MemberService.java
+++ src/main/java/com/example/board2_2024_boot/service/MemberService.java
@@ -1,7 +1,16 @@
 package com.example.board2_2024_boot.service;
 
+import com.example.board2_2024_boot.dto.MemberDTO;
+import com.example.board2_2024_boot.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public void save(MemberDTO memberDTO) {
+
+    }
 }
