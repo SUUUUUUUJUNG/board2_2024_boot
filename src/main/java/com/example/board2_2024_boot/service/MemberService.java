dfda728074e7218f5255aa13b266d1a247edbbbc
SHIN SUJUNG <shinssjj79@gmail.com>
2024-08-09 05:47:21 +0900

    feat: Implement save method in MemberService
    - Added conversion from MemberDTO to MemberEntity in the save method.
    - Called the save method of the repository with the converted entity.
    
    This commit improves the functionality by enabling the saving of member data.

--- src/main/java/com/example/board2_2024_boot/service/MemberService.java
+++ src/main/java/com/example/board2_2024_boot/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.board2_2024_boot.service;
 
 import com.example.board2_2024_boot.dto.MemberDTO;
+import com.example.board2_2024_boot.entity.MemberEntity;
 import com.example.board2_2024_boot.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,11 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public void save(MemberDTO memberDTO) {
+        // 1. dto -> entity 변환
+        // 2. repository의 save 메서드 호출
+       MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+       memberRepository.save(memberEntity);
+        //repository의 save 메서드 호출(조건. entity 객체를 넘겨줘야 함)
 
     }
 }
