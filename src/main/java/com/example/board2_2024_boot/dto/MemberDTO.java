2f1faf42b7fb6082bf5bd9cdc4260db3b377eecc
SHIN SUJUNG <shinssjj79@gmail.com>
2024-08-16 20:07:47 +0900

    Add `toMemberDTO` method in `MemberDTO` to map from `MemberEntity`
    Enhanced `MemberDTO` by adding a static method `toMemberDTO` that allows conversion from a `MemberEntity` instance to a `MemberDTO`. This improves the separation of concerns and provides a cleaner way to map entity data to DTOs.

--- src/main/java/com/example/board2_2024_boot/dto/MemberDTO.java
+++ src/main/java/com/example/board2_2024_boot/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.example.board2_2024_boot.dto;
 
+import com.example.board2_2024_boot.entity.MemberEntity;
 import lombok.*;
 
 @Getter
@@ -12,5 +13,14 @@ public class MemberDTO {
     private String memberEmail;
     private String memberPassword;
     private String memberName;
+
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity){
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        return memberDTO;
+    }
 
 }
