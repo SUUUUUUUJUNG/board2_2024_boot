21cb17b8262e532d1ef45ec8409069426322904a
SHIN SUJUNG <shinssjj79@gmail.com>
2024-08-22 05:51:07 +0900

    Add findAll method to MemberService
    - Implemented a new method `findAll` in the MemberService class to retrieve all members from the database and convert them to DTOs.
    - This method leverages the existing repository’s `findAll` call and iterates over the resulting entity list to create a list of MemberDTOs.
    - Removed commented code in the `findAll` method to maintain clean and efficient code structure.

--- src/main/java/com/example/board2_2024_boot/service/MemberService.java
+++ src/main/java/com/example/board2_2024_boot/service/MemberService.java
@@ -6,6 +6,8 @@ import com.example.board2_2024_boot.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -45,4 +47,16 @@ public class MemberService {
             return null;
         }
     }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for (MemberEntity memberEntity: memberEntityList) {
+            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+//            MemberDTO memberDTO = MemberDTO.toMemberDTO(memberEntity);
+//            memberDTOList.add(memberDTO);
+        }
+        return memberDTOList;
+    }
+    
 }
